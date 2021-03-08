7f7a61f9d39a229e23bc741a987ddbb4dd0666d0
Justin Caamaño <justincaamano133@gmail.com>
2021-03-08 20:19:45 -0300

    version 1.1

--- layouts/header.php
+++ layouts/header.php
@@ -12,7 +12,7 @@
 
             <div class="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center">
               <div class="site-logo">
-                <a href="index.php" class="js-logo-clone">Tienda Online</a>
+                <a href="index.php" class="js-logo-clone">Mi Tienda Online</a>
               </div>
             </div>
 
